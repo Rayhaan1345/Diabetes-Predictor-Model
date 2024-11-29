11f063eb492677fdb359bccfe8bedbb8ac0672fe
Rayhaan Khan <rayhaankhan1345@gmail.com>
2024-11-29 20:05:05 +0530

    Update diabetesapp.py

--- Deployment/diabetesapp.py
+++ Deployment/diabetesapp.py
@@ -10,14 +10,14 @@ st.subheader("By: Rayhaan Khan")
 
 gluc = 'https://drive.google.com/file/d/1ur8gpMcK5C09tV6GVFthQXFm8pCiwGs6/view?usp=sharing'
 
-learn = load_learner('model.pkl')
+learn = load_learner('Deployment/model.pkl')
 
 if st.button("press to run", type="primary"):
-    with open('diabetes.csv', 'a') as file:
+    with open('Deployment/diabetes.csv', 'a') as file:
         writer = csv.writer(file)
         writer.writerow([pregnancies, glucose, bloodp, SkinThickness, insulin, bmi, DiabetesPedigreeFunction, Age])
-        learn = load_learner('model.pkl')
-        test = pd.read_csv('diabetes.csv')
+        learn = load_learner('Deployment/model.pkl')
+        test = pd.read_csv('Deployment/diabetes.csv')
         dl = learn.dls.test_dl(test)
         preds = learn.get_preds(dl=dl)
         preds = [x.item() for x in preds[0]]
@@ -26,6 +26,4 @@ if st.button("press to run", type="primary"):
         st.info(preds)
 
 
-#st.title("Acknowledgements")
-#st.markdown("I would like to say a special thank you to my uncle, Dr. Shadab Khan for providing me with this wonderful opportunity, to learn and expand my knowledge of AI, Deep Learning and ML and always providing valuable feedback for my assignments and projects.")
 
