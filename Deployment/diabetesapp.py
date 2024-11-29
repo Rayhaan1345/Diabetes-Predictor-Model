dc1f84a74f668fa4bf8befafc693f3f7cbd923a5
Rayhaan Khan <rayhaankhan1345@gmail.com>
2024-11-29 20:14:35 +0530

    Update diabetesapp.py

--- Deployment/diabetesapp.py
+++ Deployment/diabetesapp.py
@@ -10,6 +10,18 @@ st.subheader("By: Rayhaan Khan")
 
 gluc = 'https://drive.google.com/file/d/1ur8gpMcK5C09tV6GVFthQXFm8pCiwGs6/view?usp=sharing'
 
+#sliders
+st.header(":green[**Lets start changing some parameters**]")
+glucose = st.slider("**Glucose amount (mg/dL) [ref. 100]**", 0, 199, 100)
+bmi = st.slider("**BMI (Body Mass Index) [ref 30]**", 0, 67, 33)
+insulin = st.slider("**Insuliin concentration(2-Hour serum insulin (mu U/ml) [ref. 80-100]**", 0, 846, 122)
+bloodp = st.slider("**Blood Pressure (mm Hg)[ref. 80, non preg.]**", 0, 140, 80)
+pregnancies = st.slider("**Amount of pregnancies (only for women)**", 0, 14, 2)
+SkinThickness = 35
+DiabetesPedigreeFunction = 0.627
+Age = st.slider("**Age**", 0, 100, 50)
+
+
 learn = load_learner('Deployment/model.pkl')
 
 if st.button("press to run", type="primary"):
